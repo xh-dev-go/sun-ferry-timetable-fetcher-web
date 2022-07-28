573a25f74b6bb3d151fe5a730b38459b1f5d9fb0
xeth hung <adamhung12@gmail.com>
2022-07-29 03:14:52 +0800

    add ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {FerryScheduleComponent} from './ferry/ferry-schedule/ferry-schedule.component';
 import { BaseComponent } from './base/base.component';
 import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {FormsModule} from "@angular/forms";
       {path: "ferry-schedule", component: FerryScheduleComponent},
       {path: "", redirectTo: "/ferry-schedule", pathMatch: "full"}
     ]),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
